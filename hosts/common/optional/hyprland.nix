432906878f57a0c857ef02d516ad609d1b78fca9
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2023-08-02 10:38:47 +0200

    feat: fixed xdg portal issue

--- hosts/common/optional/hyprland.nix
+++ hosts/common/optional/hyprland.nix
@@ -14,11 +14,9 @@
 
     xdg.portal = {
         enable = true;
-        wlr.enable = true;
 
         extraPortals = with pkgs; [
-        #    xdg-desktop-portal-gtk
-            xdg-desktop-portal-hyprland
+            xdg-desktop-portal-gtk
         ];
     };
 
