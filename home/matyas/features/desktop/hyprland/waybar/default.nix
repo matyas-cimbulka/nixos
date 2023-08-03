fe5b6d36c1c06f94dc360ae9c31b570c7e2e9133
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2023-08-03 14:50:27 +0200

    feat: adds new waybar configuration

--- home/matyas/features/desktop/hyprland/waybar/default.nix
+++ home/matyas/features/desktop/hyprland/waybar/default.nix
@@ -7,14 +7,17 @@
             mainBar = {
                 layer = "top";
                 position = "top";
-                height = 30;
+                height = 32;
 
                 modules-left = [ "hyprland/workspaces" "hyprland/window" ];
                 modules-center = [ "clock" ];
-                modules-right = [ "tray" "hyprland/language" "battery" "backlight" "network" "wireplumber" ];
+                modules-right = [ "tray" "hyprland/language" "battery" "backlight" "wireplumber" "network" ];
 
                 "wlr/workspaces" = {
                     format = "{name}";
+                    disable-scroll = true;
+                    disable-markup = false;
+                    all-outputs = true;
                 };
 
                 "hyprland/window" = {
@@ -26,19 +29,40 @@
                     format-alt = "{:%a, %d. %b  %H:%M}";
                 };
 
-                batter = {
+                tray = {
+                    icons-size = 18;
+                    spacing = 10;
+                };
+
+                battery = {
                     format = "{capacity}% {icon}";
-                    format-icons = [ "󰁻" "󰁽" "󰁿" "󰂁" "󰁹" ];
+                    format-charging = "{capacity}% ";
+                    format-plugged = "{capacity}% ";
+                    format-icons = [ "" "" "" "" "" ];
+                    states = {
+                        warning = 30;
+                        critical = 15;
+                    };
                 };
 
                 "hyprland/language" = {
                     format = "{short}";
                 };
 
+                backlight = {
+                    format = "{icon} {percent}%";
+                    format-icons = [ "" ];
+                };
+
+                wireplumber = {
+                    format = "{icon} {volume}%";
+                };
+
                 network = {
-                    format-wifi = "{essid} {icon}";
-                    format-ethernet = "{ifname}";
-                    format-disconnected = "";
+                    format-wifi = "{icon} ";
+                    format-ethernet = "{icon} ";
+                    format-disconnected = " Disconnected";
+                    tooltip-format = "{ifname}: {ipaddr}";
                     max-lenght = 50;
                     on-click = "alacritty -e 'nmtui'";
                     format-icons = [ "󰤯" "󰤟" "󰤢" "󰤥" "󰤨" ];
