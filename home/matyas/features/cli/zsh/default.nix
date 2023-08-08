89a21668a45ca62219aab8b872f3d6222593d955
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2023-08-08 11:47:12 +0200

    feat: removes oh-my-zsh

--- home/matyas/features/cli/zsh/default.nix
+++ home/matyas/features/cli/zsh/default.nix
@@ -17,19 +17,19 @@
     initExtra = "source ~/.p10k.zsh";
     syntaxHighlighting.enable = true;
 
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "sudo"
-        "systemd"
-      ];
-    };
-
     plugins = [
       {
         name = "powerlevel10k";
         src = pkgs.zsh-powerlevel10k;
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "sudo";
+        src = ./plugins/sudo.plugin.zsh;
+      }
+      {
+        name = "systemd";
+        src = ./plugins/systemd.plugin.zsh;
       }
     ];
   };
