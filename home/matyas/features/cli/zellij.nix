863ee4c658ada3ac252112463369ee96c9bb228f
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2023-08-28 08:55:17 +0200

    feat: updates config for zellij

--- home/matyas/features/cli/zellij.nix
+++ home/matyas/features/cli/zellij.nix
@@ -3,9 +3,13 @@
 {
     home.packages = [ pkgs.zellij ];
 
-    programs.zellij = {
+    programs.zellij = {      
         enable = true;
-
     #    enableZshIntegration = true;
+
+        settings = {
+            theme = "nord";
+            pane_frames = false;
+        };
     };
 }
