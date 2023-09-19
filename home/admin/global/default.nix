306a21e86ed3821720d56d44b7d89e0e826be8ac
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2023-09-19 11:29:30 +0200

    feat: adds home manager config form homelab server

---
+++ home/admin/global/default.nix
@@ -0,0 +1,30 @@
+{ inputs, lib, pkgs, config, ... }:
+
+{
+    imports = [
+        ../features/cli
+        ../feauters/helix
+    ];
+
+    nixpkgs = {
+        config = {
+            allowUnfree = true;
+            allowUnfreePredicate = (_: true);
+        };
+    };
+
+    nix = {
+        package = lib.mkDefault pkgs.nix;
+        settings = {
+            experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+            warn-dirty = false;
+        };
+    };
+
+    home = {
+        username = lib.mkDefault "admin";
+        homeDirectory = lib.mkDefault "/home/admin";
+        stateVersion = lib.mkDefault "23.05";
+        sessionPath = [ "$HOME/.local/bin" ]
+    }
+}
