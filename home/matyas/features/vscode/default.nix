77d7c9d96133d95dc7dee4f1691ded072bdc5631
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2023-10-03 11:00:46 +0200

    feat: adds extensions and user settings to the vscode configuration

--- home/matyas/features/vscode/default.nix
+++ home/matyas/features/vscode/default.nix
@@ -7,6 +7,28 @@
         extensions = with pkgs.vscode-extensions; [
             eamodio.gitlens
             bbenoist.nix
+
+            hashicorp.terraform
+
+            rust-lang.rust-analyzer
+            serayuzgur.crates
+            tamasfe.even-better-toml
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+            {
+                name = "bracket-pair-color-dlw";
+                publisher = "BracketPairColorDLW";
+                version = "0.0.6";
+                sha256 = "WB929LJWf7jbCkWoNnQaSkQxETxN4xtJJHwsLk2+dmw=";
+            }
         ];
+
+        userSettings = {
+            "debug.console.fontFamily" = "FiraMono Nerd Font";
+            "terminal.integrated.fontFamily" = "FiraMono Nerd Font";
+            "explorer.confirmDelete" = false;
+            "[terraform]" = {
+                "editor.tabSize" = 2;
+            };
+        };
     };
 }
