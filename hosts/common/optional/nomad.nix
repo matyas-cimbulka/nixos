3d9c721769eeb864e1281f5bcffee803111662f2
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2024-02-27 10:22:51 +0100

    feat: modifies nomad configuration

--- hosts/common/optional/nomad.nix
+++ hosts/common/optional/nomad.nix
@@ -6,12 +6,22 @@
             server = {
                 enabled = true;
                 bootstrap_expect = 2;
+
+                server_join = {
+                    retry_join = [ "192.168.50.5" "192.16.50.6" ];
+                    retry_max = 3;
+                    retry_interval = "15s";
+                };
+            };
+
+            ui = {
+                enabled = true;
             };
 
             client = {
                 enabled = true;
-                servers = [ "10.0.10.2" "10.0.10.3" ];
-                network_interface = "vlan10@end0";
+                servers = [ "192.168.50.5" "192.16.50.6" ];
+#                network_interface = "vlan10@end0";
             };
         };
     };
