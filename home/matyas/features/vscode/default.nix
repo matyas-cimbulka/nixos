5106b2b00e204c52119ab8297a281d3137961938
Matyas Cimbulka <matyas.cimbulka@gmail.com>
2024-08-06 12:29:27 +0200

    feat: changes VS Code title bar to custom

--- home/matyas/features/vscode/default.nix
+++ home/matyas/features/vscode/default.nix
@@ -31,6 +31,7 @@
             "terminal.external.linuxExec" = "warp-terminal";
             "workbench.iconTheme" = "material-icon-theme";
             "editor.formatOnSave" = true;
+            "window.titleBarStyle" = "custom";
             "rust-analyzer.check.command" = "clippy";
 #            "editor.defaultFormatter" = "esbenp.prettier-vscode";
             "[svelte]" = {
