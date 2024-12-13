25dbb46a908dd7429eb47bddd94fd53214d134ef
Matyáš Cimbulka <matyas.cimbulka@gmail.com>
2024-12-13 14:29:07 +0100

    chore: updates flake inputs

--- home/matyas/features/desktop/common/default.nix
+++ home/matyas/features/desktop/common/default.nix
@@ -1,12 +1,8 @@
 { pkgs, ... }:
 {
     home.packages = with pkgs; [
-        # anytype
         # _1password-gui-beta
-        blackbox-terminal
-        # gitbutler
         google-chrome
         yubioath-flutter
-        # warp-terminal
     ];
 }
